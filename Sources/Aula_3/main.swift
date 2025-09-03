4ee34addd9185019eb8979e90d23bb02fe214c2c
David Chen <davidchen@cweb.com.br>
2025-09-03 18:40:17 +0000

    Aula 3

--- Sources/Aula_3/main.swift
+++ Sources/Aula_3/main.swift
@@ -1,3 +1,37 @@
 import Foundation
 
-print("Aula 3 de Swift!!")
+class Carro{
+    private(set) var marca: String
+    private(set) var modelo: String
+    init(marca: String, modelo: String) {
+        self.marca = marca
+        self.modelo = modelo
+    }
+}
+public func acelerar(){
+    print(") carro está acelerando")
+}
+public func frear(){
+    print("O carros está freando")
+}
+
+public func getMarca() -> String{
+    self.marca = marca
+}
+
+private func setMarca(marca: String{
+    self.marca = marca
+}
+
+public func alterMarca(senha: Int, novamarca: String){
+    if senha == 123{
+        self.setMarca(marca: novaMarca)
+    }
+}
+
+var carro = Carro(marca: "Ford", modelo: "Focus")
+
+print(carro.marca)
+print()
+carro.setMarca(marca: "Volkswagen")
+
